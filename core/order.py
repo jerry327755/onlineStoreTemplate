c8bae6b5e72b951ca3640486760db0ecf0f06b2a
Sprocket <lpolar45@gmail.com>
2023-07-24 22:01:09 -0400

    p2.md - part 2, updated class diagrams

---
+++ core/order.py
@@ -0,0 +1,21 @@
+
+
+class Order:
+    
+    def __init__(self, items):
+        pass
+    
+    def get_items() -> list:
+        pass
+    
+    def add_item(item, amount):
+        pass
+    
+    def remove_item(item_id, amount):
+        pass
+
+    def get_order_total() -> float:
+        pass
+    
+    def finalize_order():
+        pass
---
+++ core/product.py
@@ -0,0 +1,35 @@
+# imports
+
+class Product:
+    """
+    Product is a class that represents a product sold by the store.
+
+    args:
+        - product_name: the name of the product
+        - price: the price of the product
+        - inventory: the current amount of the product in inventory 
+    """
+
+    def __init__(self, product_name: str, price: float, inventory:int) -> None:
+        pass
+    
+    def get_id() -> int:
+        pass
+
+    def get_name():
+        pass
+    
+    def set_name(name: str):
+        pass
+    
+    def get_price():
+        pass
+    
+    def set_price(new_price: float):
+        pass
+    
+    def get_inventory():
+        pass
+    
+    def set_inventory(new_inventory: int):
+        pass
