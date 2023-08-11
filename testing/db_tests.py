14d2189539d160895200293218aba9f206770cb8
Gerardo Rodriguez <114891176+jerry327755@users.noreply.github.com>
2023-08-10 22:12:22 -0400

    product category test

--- testing/db_tests.py
+++ testing/db_tests.py
@@ -86,7 +86,8 @@ def test_check_connection_threaded(db: Database = None) -> tuple:
         return False, error
     else:
         return True, "Connection is not single threaded."
-    
+
+
 def test_stock_filtered(db: Database = None) -> tuple:
     """
     Tests that the stock has been filtered from low-to-high and high-to-low.
@@ -98,20 +99,86 @@ def test_stock_filtered(db: Database = None) -> tuple:
         - error_report: a tuple containing a boolean and a string,
     """
     db = Database("database/store_records.db") if db is None else db
-    
-    products = db.get_full_inventory()  
+
+    products = db.get_full_inventory()
     low_to_high = sorted(products, key=lambda x: x['price'])
     high_to_low = sorted(products, key=lambda x: x['price'], reverse=True)
-    
+
     for i in range(len(low_to_high) - 1):
         if low_to_high[i]['price'] > low_to_high[i + 1]['price']:
             error = f"Error in test_stock_filtered: Low_to_high stock is not filtered.\n  - Actual: False"
             return False, error
-        
+
     for i in range(len(high_to_low) - 1):
         if high_to_low[i]['price'] < high_to_low[i + 1]['price']:
             error = f"Error in test_stock_filtered: High_to_low stock is not filtered.\n  - Actual: False"
             return False, error
-        
+
     return True, "Stock is filtered successfully."
-        
+
+
+"""
+This test ensures that each product is obtained based on its product category using a database.
+
+"""
+
+
+
+
+def test_product_category():
+    """
+    Retrieves products based on their categories from the database.
+
+    Retrieves items for each category and performs individual category tests.
+
+    Returns:
+        bool: True if all tests passed, False is tests failed.
+    """
+    db = Database('database/store_records.db')
+
+    def test_each_category(category, elements):
+        """
+        This test method verifies if the items in a category have the correct category.
+
+        Args:
+            category (STR): The category that each element should be in.
+            elements (LIST): Product List
+
+        Returns:
+            bool: True if all items in the category have the correct category, False if products do not have the correct category.
+        """
+        for item in elements:
+            if item['category'] != category:
+                print("test_product_category test failed for category:", category)
+                return False
+        print('test_product_category test passed for category:', category)
+        return True
+
+    #Product categories
+    FRUITS = 'Fruit'
+    VEGETABLES = 'Vegetables'
+    SEEDS = 'Seeds'
+    DIARY = 'Diary'
+    # Test Fruits category
+    elements = db.get_items_by_category(FRUITS)
+    if not test_each_category(FRUITS, elements):
+        return False, "All tests failed"
+
+    # Test Vegetables category
+    elements = db.get_items_by_category(VEGETABLES)
+    if not test_each_category(VEGETABLES, elements):
+        return False, "All tests failed"
+
+    # Test Seeds category
+    elements = db.get_items_by_category(SEEDS)
+    if not test_each_category(SEEDS, elements):
+        return False, "All tests failed"
+
+    # Test Dairy category
+    elements = db.get_items_by_category(DIARY)
+    if not test_each_category(DIARY, elements):
+        return False, "All tests failed"
+
+    return True, "All tests Passed"
+
+
---
+++ testing/reviews/grodri29_review.md
@@ -0,0 +1,10 @@
+# Testing Review
+
+## Tests Reviewed
+
+- **Test Source File:** [tests/db_tests.py](../../tests/db_tests.py)
+  - **Test Function Name:** `test_stock_filtered`
+    - **Date Reviewed:** 08/10/2023
+    - **Comments:**
+      - This test ensure that products are ordered from high-to-low and low-to-high by prices and works correctly.
+
